5695d76b411dce14178dbf37378b248d315a0167
Svetozar99 <svetozarbrboric@gmail.com>
2021-04-21 22:02:44 +0200

    inicijalni commit

---
+++ InformacionaBezbjednost/src/main/java/com/bezbjednost/InformacionaBezbjednost/InformacionaBezbjednostApplication.java
@@ -0,0 +1,13 @@
+package com.bezbjednost.InformacionaBezbjednost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacionaBezbjednostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacionaBezbjednostApplication.class, args);
+	}
+
+}
---
+++ InformacionaBezbjednost/src/test/java/com/bezbjednost/InformacionaBezbjednost/InformacionaBezbjednostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezbjednost.InformacionaBezbjednost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformacionaBezbjednostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
